55231489c247d1bdd1ef654614039d54a9e02571
Michael Ducharm <ducharmdev@gmail.com>
2021-07-27 21:22:34 -0400

    added some initial policies, added group.created_by

--- db/init/103-group.sql
+++ db/init/103-group.sql
@@ -3,6 +3,7 @@
 create table if not exists app_public.group (
     id serial primary key,
     name text not null check (char_length(name) < 80),
+    created_by integer references app_public.person,
     created_at timestamp default now(),
     updated_at timestamp default now() 
 );
---
+++ db/init/400-policies.sql
@@ -0,0 +1,36 @@
+\connect shrimp_points_db;
+
+-- * Enable RLS
+alter table app_public.person enable row level security;
+alter table app_public.group enable row level security;
+alter table app_public.group_invite enable row level security;
+alter table app_public.feed_entry enable row level security;
+alter table app_public.task enable row level security;
+alter table app_public.task_rating enable row level security;
+alter table app_public.task_completed enable row level security;
+
+
+-- * select
+create policy select_person on app_public.person for select using (true);
+create policy select_group on app_public.group for select using (true);
+create policy select_group_invite on app_public.group_invite for select using (true);
+create policy select_feed_entry on app_public.feed_entry for select using (true);
+create policy select_task on app_public.task for select using (true);
+create policy select_task_rating on app_public.task_rating for select using (true);
+create policy select_task_completed on app_public.task_completed for select using (true);
+
+
+-- * insert
+create policy insert_group on app_public.group for insert to sp_person
+    with check(created_by = current_person_id()
+        and len(select id from app_public.group where created_by = current_person_id()) = 0);
+
+
+-- * update
+create policy update_person on app_public.person for update to sp_person
+    using (id = nullif(current_setting('jwt.claims.person_id', true), '')::integer);
+
+
+-- * delete
+create policy delete_person on app_public.person for delete to sp_person
+    using (id = nullif(current_setting('jwt.claims.person_id', true), '')::integer);
