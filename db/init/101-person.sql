8140ec7a7292d168176ae323e0a0821b691a3ddb
Michael Ducharm <ducharmdev@gmail.com>
2021-07-27 21:32:45 -0400

    removed old comments

--- db/init/101-person.sql
+++ db/init/101-person.sql
@@ -10,8 +10,7 @@ create table if not exists app_public.person (
 
 comment on table app_public.person is 'A user of the forum.';
 comment on column app_public.person.id is 'The primary unique identifier for the person.';
-comment on column app_public.person.first_name is 'The person’s first name.';
-comment on column app_public.person.last_name is 'The person’s last name.';
+comment on column app_public.person.display_name is 'Username that other users see.';
 comment on column app_public.person.about is 'A short description about the user, written by the user.';
 comment on column app_public.person.created_at is 'The time this person was created.';
 comment on column app_public.person.updated_at is 'The time this person was updated.';
