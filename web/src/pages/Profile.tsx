77a072d8ac921be446182b1044a3e84cc7d44720
Michael Ducharm <ducharmdev@gmail.com>
2021-08-03 22:31:17 -0400

    added profile

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -5,21 +5,21 @@ import { ALL_PEOPLE } from '../graphql/queries';
 import { useQuery } from '@apollo/client';
 import { useEffect } from 'react';
 
-function Home() {
-  // const { loading, error, data } = useQuery(ALL_PEOPLE);
+function Profile() {
+  const { loading, error, data } = useQuery(ALL_PEOPLE);
 
-  // useEffect(() => console.log(data))
+  useEffect(() => console.log(data))
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* {loading && <p>loading...</p>}
-        {error && <p>error: {error.message}</p>} */}
-        <p>Home</p>
+        {loading && <p>loading...</p>}
+        {error && <p>error: {error.message}</p>}
+        <p>Profile</p>
       </header>
     </div>
   )
 }
 
-export default Home
+export default Profile
