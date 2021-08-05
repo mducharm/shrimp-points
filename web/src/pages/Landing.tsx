87fbffde7e1925c83f4d496be6a6af1de9590229
Michael Ducharm <ducharmdev@gmail.com>
2021-08-04 23:07:37 -0400

    remove menu on landing

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import logo from "../logo.svg";
 import "../App.css";
 import Main from "../layouts/Main";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <Main>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Landing</p>
-        </header>
-      </div>
-    </Main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Landing</p>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </header>
+    </div>
   );
 }
 
