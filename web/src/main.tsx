0065b5a8484f9416141df2f4e775fcd1efe78254
Michael Ducharm <ducharmdev@gmail.com>
2021-08-04 22:57:51 -0400

    added theme, disable strict mode for menu

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,19 +4,42 @@ import "./index.css";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Store } from "./store/store";
+import { ThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
 });
 
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#62727b',
+      main: '#37474f',
+      dark: '#102027',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ff867f',
+      main: '#ff5252',
+      dark: '#c50e29',
+      contrastText: '#000000',
+    },
+  },
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ApolloProvider client={client}>
       <Store>
+        <ThemeProvider theme={theme}>
         <App />
+        </ThemeProvider>
       </Store>
     </ApolloProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>
+  ,
   document.getElementById("root")
 );
