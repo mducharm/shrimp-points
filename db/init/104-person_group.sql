23341469dc85429b3549496cff4111e0e7e813ee
Michael Ducharm <ducharmdev@gmail.com>
2021-08-07 15:10:21 -0400

    added person_group trigger

--- db/init/104-person_group.sql
+++ db/init/104-person_group.sql
@@ -5,3 +5,15 @@ create table if not exists app_public.person_group (
     group_id integer references app_public.group,
     primary key (person_id, group_id)
 );
+
+
+create function app_private.create_person_group() returns trigger as $$
+    begin 
+        insert into app_public.person_group (person_id, group_id)
+        values (new.created_by, new.id);
+        return new;
+    end;
+$$ language plpgsql;
+
+
+create trigger group_created_with_person_group after insert on app_public.group for each row execute procedure app_private.create_person_group();
