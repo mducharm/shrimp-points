b1207d350c95da625fbf105ee94c6f1c508fcc8d
Michael Ducharm <ducharmdev@gmail.com>
2021-08-10 20:16:14 -0400

    set up autocomplete; need to implement debouncing

--- web/src/components/SendInvite.tsx
+++ web/src/components/SendInvite.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { TextField, Button } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SEND_INVITE } from "../graphql/mutations";
 import { SEARCH_PEOPLE } from "../graphql/queries";
 import { defaultMutationOptions } from "../helper";
@@ -9,28 +9,45 @@ import { Person } from "../hooks/useGroupManager";
 
 function SendInvite() {
   const [search, setSearchText] = useState("");
-  const [person, setPerson] = useState({} as Person);
-  const { loading, error, data } = useQuery(SEARCH_PEOPLE, {
+  const [person, setPerson] = useState<Person | null>(null);
+  const { loading, error, data, refetch } = useQuery(SEARCH_PEOPLE, {
     variables: { search },
     ...defaultMutationOptions,
   });
   //   const { loading, error, data } = useQuery(SEND_INVITE);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  useEffect(() => {
+    refetch();
+  }, [search]);
 
-  let people = data?.searchPeople?.nodes;
+  // useEffect(() => {
+  //   console.log(person);
+  // }, [person]);
+
+  let people = data?.searchPeople?.nodes ?? [];
 
   return (
     <>
       <Autocomplete
-        id="combo-box-demo"
+        id="search-users"
         options={people}
         getOptionLabel={(option: any) => option.displayName}
         style={{ width: 300 }}
+        onInputChange={(event: any, value: string) => {
+          setSearchText(value);
+        }}
+        inputValue={search}
+        value={person}
+        onChange={(event: any, newValue: Person | null) => {
+          if (newValue) setPerson(newValue);
+        }}
+        getOptionSelected={(option, value) =>
+          value != null &&
+          option.displayName === value.displayName &&
+          option.id === value.id
+        }
         renderInput={(params: any) => {
-          console.log(params);
-          return <TextField {...params} label="Combo box" variant="outlined" />;
+          return <TextField {...params} label="User" variant="outlined" />;
         }}
       />
 
